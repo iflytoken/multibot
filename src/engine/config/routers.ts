c61f940f04682c5f91138118a65eaaed8db9b8db
iflytoken <83847571+iflytoken@users.noreply.github.com>
2025-12-07 12:50:39 +1300

    Update routers.ts

--- src/engine/config/routers.ts
+++ src/engine/config/routers.ts
@@ -1,1 +1,20 @@
+export const ROUTERS = [
+  // PancakeSwap V2 Testnet
+  { name: "Pancake V2", address: "0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3" },
+
+  // ApeSwap
+  { name: "ApeSwap", address: "0xB2b67540cF4dF9aD3CE7F6e9Bb40d4b7CdA8dD54" },
+
+  // Biswap
+  { name: "Biswap", address: "0x3A6D8c2c9A1f0896B4BE72aB8C58e0a604D4e5bA" },
+
+  // MDEX clone test router
+  { name: "Mdex", address: "0xD99D1c33F9fC3444f8101754aBC46c52416550D1" },
+
+  // BabyDoge
+  { name: "BabyDoge", address: "0x3BcFD7A62e0CbCfD9BfD0eA497dc6768F1dFf57e" },
+
+  // Thena (test router)
+  { name: "Thena", address: "0xD5c031c5D7AaF6e13067C62c5390b25A1CdBa41C" }
+];
 
