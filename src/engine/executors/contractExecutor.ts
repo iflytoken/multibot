543bb02e9233c99f9c952a91eae0c40909080f47
iflytoken <83847571+iflytoken@users.noreply.github.com>
2025-12-07 12:53:37 +1300

    Update contractExecutor.ts

--- src/engine/executors/contractExecutor.ts
+++ src/engine/executors/contractExecutor.ts
@@ -1,1 +1,11 @@
 
+import { ethers } from "ethers";
+import ArbExecutorABI from "../../abi/ArbExecutor.json";
+
+export async function executePlan(plan, provider, privateKey, contractAddress) {
+  const signer = new ethers.Wallet(privateKey, provider);
+  const contract = new ethers.Contract(contractAddress, ArbExecutorABI, signer);
+
+  const tx = await contract.executeArb(plan);
+  return tx.wait();
+}
